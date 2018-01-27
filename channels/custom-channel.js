fdcc6fbc91ba2860885b48fde09a2a7d80c56fa6
Leonid Beder <leonid.beder@gmail.com>
2018-01-27 16:19:18 +0200

    Implement external custom channel support.

---
+++ channels/custom-channel.js
@@ -0,0 +1,59 @@
+var ChannelInterface = require('./channel-interface')
+  , Joi = require('joi')
+  , prettifyJoiError = require('../helpers/prettify-joi-error')
+  , util = require('util')
+  , _ = require('lodash')
+
+function CustomChannel(opts) {
+  var validatedOptions = Joi.validate(opts || {}, Joi.object().keys({
+    channelOptions: Joi.object().keys({
+      connector: Joi.object()
+    })
+    , logFunction: Joi.func()
+    , id: Joi.string()
+  }).requiredKeys('channelOptions', 'channelOptions.connector'), {
+      convert: false
+    })
+
+  ChannelInterface.apply(this, Array.prototype.slice.call(arguments))
+
+  if (validatedOptions.error != null) {
+    throw new Error(prettifyJoiError(validatedOptions.error))
+  }
+
+  this._connector = _.get(validatedOptions, 'value.channelOptions.connector')
+}
+
+util.inherits(CustomChannel, ChannelInterface)
+
+CustomChannel.prototype._connect = function _connect() {
+  const self = this;
+
+  this._connector.connect();
+  this._connected()
+
+  this._connector.on('received', function received(originNodeId, message) {
+    self._recieved(originNodeId, message);
+  });
+}
+
+CustomChannel.prototype._disconnect = function _disconnect() {
+  this._connector._disconnect();
+  this._disconnected()
+}
+
+CustomChannel.prototype._broadcast = function _broadcast(data) {
+  this._connector.broadcast({
+    from: this.id, data: data
+  });
+}
+
+CustomChannel.prototype._send = function _send(nodeId, data) {
+  this._connector.send(nodeId, {
+    from: this.id,
+    to: nodeId,
+    data: data
+  });
+}
+
+module.exports = CustomChannel;
--- channels/index.js
+++ channels/index.js
@@ -2,4 +2,5 @@ module.exports = {
   memory: require('./in-memory-channel')
 , 'socket.io': require('./socket-io-channel')
 , redis: require('./redis-channel')
+, custom: require('./custom-channel')
 }
